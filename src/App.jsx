c02b95b7a96f6aff75aa584e2714a11116efa4c6
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-02-06 10:32:22 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <>
+      <h1 className="uppercase text-4xl">Vite + React</h1>
+      <button className="btn btn-info">Info</button>
+      <button className="btn btn-success">Success</button>
+      <button className="btn btn-warning">Warning</button>
+      <button className="btn btn-error">Error</button>
+
+    </>
+  );
+}
+
+export default App;
