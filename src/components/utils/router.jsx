643f2ebb7ea1e88c42562e14c5875d7996884357
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-03-13 18:49:53 +0600

    added new file and folder

--- src/components/utils/router.jsx
+++ src/components/utils/router.jsx
@@ -1,30 +1,34 @@
-import { createBrowserRouter } from "react-router-dom";
+import {
+    createBrowserRouter,
+    
+  } from "react-router-dom";
 import MainLayout from "./MainLayout/MainLayout";
 import Home from "../Home/Home";
 import Login from "../Login/Login";
 import NewAccount from "../NewAccount/NewAccount";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <MainLayout></MainLayout>,
-    children: [
-      {
-        path: "/",
-        element: <Home></Home>,
-      },
-      {
-        path: "/login",
-        element: <Login></Login>,
-        children: [
-          {
-            path: "/newAcount",
-            element: <NewAccount></NewAccount>,
-          },
-        ],
-      },
-    ],
-  },
-]);
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <MainLayout></MainLayout>, 
+      children: [
+        {
+            path: '/',
+            element: <Home></Home>    
+        }, 
+        {
+            path: '/login',
+            element: <Login></Login>,
+            children: [
+                {
+                    path: '/newAcount', 
+                    element: <NewAccount></NewAccount>
+                }
+            ]
+        }, 
+
+      ]
+    },
+  ]);
 
 export default router;
