7abbba88b28a940d38cf23a776f73141c3747790
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-03-14 14:28:52 +0600

    added new file and folder

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,17 +1,42 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import logo from "../../assets/assets/icons/logo.png"
+import logo from "../../assets/assets/icons/logo.png";
 
 const Navbar = () => {
-  const NavLinks = <>
-  
-  <Link to='/'><li><a>Home</a></li></Link>
-  <Link to='/portfolio'><li><a>Our Portfolio</a></li></Link>
-  <Link to='/team'><li><a>Our Team</a></li></Link>
-  <Link to='/contact'><li><a>Contact Us</a></li></Link>
-  <Link to='/service'><li><a>Services</a></li></Link>
-  <Link to='/admin'><li><a>Admin</a></li></Link>
-  </>;
+  const NavLinks = (
+    <>
+      <Link to="/">
+        <li>
+          <a>Home</a>
+        </li>
+      </Link>
+      <Link to="/portfolio">
+        <li>
+          <a>Our Portfolio</a>
+        </li>
+      </Link>
+      <Link to="/team">
+        <li>
+          <a>Our Team</a>
+        </li>
+      </Link>
+      <Link to="/contact">
+        <li>
+          <a>Contact Us</a>
+        </li>
+      </Link>
+      <Link to="/service">
+        <li>
+          <a>Services</a>
+        </li>
+      </Link>
+      <Link to="/admin">
+        <li>
+          <a>Admin</a>
+        </li>
+      </Link>
+    </>
+  );
   return (
     <div className="navbar bg-base-100 shadow-sm  mt-3 mb-6">
       <div className="navbar-start">
@@ -41,14 +66,16 @@ const Navbar = () => {
           </ul>
         </div>
         <a className="btn btn-ghost text-xl">
-            <img className="w-28 h-16" src={logo} alt="" />
+          <img className="w-28 h-16" src={logo} alt="" />
         </a>
       </div>
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1">{NavLinks}</ul>
       </div>
       <div className="navbar-end">
-        <Link to='/login'><a className="btn btn-secondary">Login</a></Link>
+        <Link to="/login">
+          <a className="btn btn-secondary">Login</a>
+        </Link>
       </div>
     </div>
   );
