2a01a3d522ab8494bd3048693234a92cc219c524
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-03-15 06:47:45 +0600

    added new file and folder

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,10 +4,10 @@ import cardImg1 from "../../assets/assets/icons/Group 1372.png";
 import cardImg2 from "../../assets/assets/icons/Group 1373.png";
 import cardImg3 from "../../assets/assets/icons/Group 1374.png";
 import cardImg4 from "../../assets/assets/images/engin-akyurt-g-m8EDc4X6Q-unsplash 1.png";
-import people1 from "../../assets/assets/images/Ellipse 90.png"
-import people2 from "../../assets/assets/images/Ellipse 91.png"
-import people3 from "../../assets/assets/images/Ellipse 92.png"
-import rating from "../../assets/assets/icons/Group 33040.png"
+import people1 from "../../assets/assets/images/Ellipse 90.png";
+import people2 from "../../assets/assets/images/Ellipse 91.png";
+import people3 from "../../assets/assets/images/Ellipse 92.png";
+import rating from "../../assets/assets/icons/Group 33040.png";
 
 const Home = () => {
   return (
@@ -132,51 +132,94 @@ const Home = () => {
       {/* Fourth Section */}
       <section className="bg-white p-10 rounded-lg border border-gray-200">
         <h2 className="text-3xl font-bold text-center mb-10">Testimonials</h2>
+        {/* People -1 */}
         <div className="flex justify-around">
-          <div className="text-center w-1/3 px-4">
-            <img
-              src={people1}
-              alt="Nash Patrik"
-              className="w-16 h-16 rounded-full mx-auto mb-4"
-            />
-            <h3 className="text-lg font-semibold">Nash Patrik</h3>
-            <p className="text-gray-500 mb-4">CEO, Manpol</p>
+          <div className=" w-1/3 px-4">
+            <div className="flex gap-5">
+              <div>
+                <img
+                  src={people1}
+                  alt="Nash Patrik"
+                  className="w-16 h-16 rounded-full mx-auto mb-4"
+                />
+              </div>
+              <div>
+                <h3 className="text-lg font-semibold">Nash Patrik</h3>
+                <p className="text-gray-500 mb-4">CEO, Manpol</p>
+              </div>
+            </div>
+
             <p className="text-gray-600 mb-4">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Purus
               commodo ipsum duis laoreet maecenas. Feugiat
             </p>
-            <img src={rating} alt="Rating" className="mx-auto" />
+            <div className="flex justify-normal">
+              <img src={rating} alt="Rating" className="mx-1 w-6 h-6" />
+              <img src={rating} alt="Rating" className="mx-1 w-6 h-6" />
+              <img src={rating} alt="Rating" className="mx-1 w-6 h-6" />
+              <img src={rating} alt="Rating" className="mx-1 w-6 h-6" />
+              <img src={rating} alt="Rating" className="mx-1 w-6 h-6" />
+            </div>
           </div>
 
-          <div className="text-center w-1/3 px-4">
-            <img
-              src={people2}
-              alt="Miriam Barron"
-              className="w-16 h-16 rounded-full mx-auto mb-4"
-            />
-            <h3 className="text-lg font-semibold">Miriam Barron</h3>
-            <p className="text-gray-500 mb-4">CEO, Manpol</p>
+          {/* People-2 */}
+          <div className=" w-1/3 px-4">
+            <div className="flex gap-5">
+              <div>
+                <img
+                  src={people2}
+                  alt="Miriam Barron"
+                  className="w-16 h-16 rounded-full mx-auto mb-4"
+                />
+              </div>
+              <div>
+                <h3 className="text-lg font-semibold">Miriam Barron</h3>
+                <p className="text-gray-500 mb-4">CEO, Manpol</p>
+              </div>
+            </div>
+
             <p className="text-gray-600 mb-4">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Purus
               commodo ipsum duis laoreet maecenas. Feugiat
             </p>
-            <img src="" alt="Rating" className="mx-auto" />
+            <div className="flex">
+              <img src={rating} alt="Rating" className="mx-1 w-6 h-6" />
+              <img src={rating} alt="Rating" className="mx-1 w-6 h-6" />
+              <img src={rating} alt="Rating" className="mx-1 w-6 h-6" />
+              <img src={rating} alt="Rating" className="mx-1 w-6 h-6" />
+              <img src={rating} alt="Rating" className="mx-1 w-6 h-6" />
+            </div>
           </div>
 
-          <div className="text-center w-1/3 px-4">
-            <img
-              src={people3}
-              alt="Bria Malone"
-              className="w-16 h-16 rounded-full mx-auto mb-4"
-            />
-            <h3 className="text-lg font-semibold">Bria Malone</h3>
-            <p className="text-gray-500 mb-4">CEO, Manpol</p>
+          {/* People-3 */}
+          <div className=" w-1/3 px-4">
+            <div className="flex gap-5">
+              <div>
+                <img
+                  src={people3}
+                  alt="Bria Malone"
+                  className="w-16 h-16 rounded-full mx-auto mb-4"
+                />
+              </div>
+              <div>
+                <h3 className="text-lg font-semibold">Bria Malone </h3>
+                <p className="text-gray-500 mb-4">CEO, Manpol</p>
+              </div>
+            </div>
+
             <p className="text-gray-600 mb-4">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Purus
               commodo ipsum duis laoreet maecenas. Feugiat
             </p>
-            <img src="" alt="Rating" className="mx-auto" />
+            <div className="flex justify-normal">
+              <img src={rating} alt="Rating" className="mx-auto w-8 h-8" />
+              <img src={rating} alt="Rating" className="mx-auto w-8 h-8" />
+              <img src={rating} alt="Rating" className="mx-auto w-8 h-8" />
+              <img src={rating} alt="Rating" className="mx-auto w-8 h-8" />
+              <img src={rating} alt="Rating" className="mx-auto w-8 h-8" />
+            </div>
           </div>
+          {/*  */}
         </div>
         <div className="flex justify-center mt-6 gap-2">
           <span className="w-3 h-3 bg-gray-300 rounded-full"></span>
